7310939c3782c68cc00f648758bc833a608b564f
SidorkinAlex <palach.orl@mail.ru>
2021-06-22 23:32:48 +0300

    add connection to Postgresql and dockerRunFile

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.2
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
 )
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,9 +1,12 @@
 package apiserver
 
+import "github.com/SidorkinAlex/go-rest-api/internal/app/store"
+
 // Config ...
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
 	LogLavel string `loml:"log_level"`
+	Store    *store.Config
 }
 
 // NewConfig ...
@@ -11,5 +14,6 @@ func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8080",
 		LogLavel: "debug",
+		Store:    store.NewConfig(),
 	}
 }
